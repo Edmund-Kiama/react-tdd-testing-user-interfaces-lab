6fab7543a167b3f19e6f750b69912ea1c87b3952
Edmund <edmundgachanja@gmail.com>
2025-02-04 10:50:04 +0300

    Completes lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React from "react";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Edmund</h1>
+      <img
+        src="https://i.pinimg.com/236x/b9/f7/01/b9f70124c3752c06b8b8bd6105385c26.jpg"
+        alt="Edmund's profile picture"
+      />
+
+      <h2>About Me</h2>
+
+      <p>I'm Edmund, a passionate developer who loves learning and building projects!</p>
+
+      <a href="https://github.com/Edmund-Kiama" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+      <a href="https://linkedin.com/in/edmund-kiama-2269a4325/" target="_blank" rel="noopener noreferrer">
+        LinkedIn
+      </a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,63 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm Edmund`", () => {
+
+    render(<App />);
+ 
+    const topLevelHeading = screen.getByRole("heading", {
+      name: /hi, i'm edmund/i,
+      exact: false,
+      level: 1,
+    });
+  
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+
+  test("displays an image of yourself", () => {
+    render(<App />);
+  
+    const image = screen.getByAltText("Edmund's profile picture");
+  
+    expect(image).toHaveAttribute("src", "https://i.pinimg.com/236x/b9/f7/01/b9f70124c3752c06b8b8bd6105385c26.jpg");
+  });
+  
+  test("displays second-level heading with the text `About Me`", () => {
+    render(<App />);
+  
+    const secondLevelHeading = screen.getByRole("heading", {
+      name: /about me/i,
+      level: 2,
+    });
+  
+    expect(secondLevelHeading).toBeInTheDocument();
+  });
+  
+  test("displays a paragraph for your biography", () => {
+    render(<App />);
+  
+    const bio = screen.getByText(/a passionate developer who loves learning/i);
+  
+    expect(bio).toBeInTheDocument();
+  });
+  
+  test("displays the correct links", () => {
+    render(<App />);
+  
+    const githubLink = screen.getByRole("link", {
+      name: /github/i,
+    });
+    const linkedinLink = screen.getByRole("link", {
+      name: /linkedin/i,
+    });
+  
+    expect(githubLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://github.com")
+    );
+  
+    expect(linkedinLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://linkedin.com")
+    );
+  });
